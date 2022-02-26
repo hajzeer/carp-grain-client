8b98ccbc22fd8cba4baf539f0c554f7bc8bdd77a
hajzeer <krzysh98@gmail.com>
2022-02-26 20:41:18 +0100

    fix cart

--- utils/client.js
+++ utils/client.js
@@ -3,8 +3,8 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_API_VERSION,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: true,
 });
